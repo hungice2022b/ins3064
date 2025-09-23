59aa5b9e27e1149988162018638c61bc1ffe5a7b
hungice2022b <22071016@vnu.edu.vn>
2025-09-23 17:55:02 +0700

    .

---
+++ lesson4.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>  
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <?php
+    //http://ins3064.test/lesson4.php?x=5&y=8
+    $x = $_GET['x']; 
+    $y = $_GET['y'];
+    echo "x: " .$x . "<br/>";
+    echo "y: " .$y . "<br/>";
+    echo "x/y : " .($x/$y) . "<br/>";
+    echo "x%y : " .($x%$y) . "<br/>";
+    echo "x++ : " .($x++) . "<br/>";
+    echo "++y : " .(++$y) . "<br/>";
+    echo "x == y : " .($x==$y) . "<br/>";
+    echo "x != y : " .($x!=$y) . "<br/>";
+    echo "x < y  : " .($x<$y) . "<br/>";
+    echo "x > y  : " .($x>$y) . "<br/>";
+    echo "x <= y : " .($x<=$y) . "<br/>";
+    echo "x >= y : " .($x>=$y) . "<br/>";
+    ?>
+</body>
+</head>
