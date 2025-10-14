d7e7d24142128c84c8e853260a61afb58759f523
hungice2022b <22071016@vnu.edu.vn>
2025-10-14 17:01:32 +0700

    .

--- week4/login.php
+++ week4/login.php
@@ -1,22 +1,24 @@
 <?php
-include("db_connect.php");
- 
-if (isset($_POST['register'])) {
-    $username = $_POST['username'];
-    $password = md5($_POST['password']);
- 
-    $check = mysqli_query($link, "SELECT * FROM users WHERE username='$username'");
-    if (mysqli_num_rows($check) > 0) {
-        echo "Existed";
-    } else {
-        
-        $query = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
-        if (mysqli_query($link, $query)) {
-            echo "Successful";
-        } else {
-            echo "Error " . mysqli_error($link);
-        }
-    }
+ include("db_connect.php");
+
+ if (isset($ POST['login'])) {
+
+   $usernane = $_POST[ 'usernane'];
+
+   $password = mđ5($_POST[ 'passuord' ]);
+
+
+
+  $query = "SELECT FROM users MHERE usernane='$username' AND password='$password";
+  $result = mysqli_ query($1ink, $query);
+
+
+
+  if (mysqli_num rows(§result) == 1) {
+     echo("Login successful!");
+  } else {
+     echo "Wrong infor";
+  }
 }
 ?>
 <!DOCTYPE html>
